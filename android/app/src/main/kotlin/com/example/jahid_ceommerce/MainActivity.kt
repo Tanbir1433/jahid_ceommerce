9259834c5b8c661315a3b76ba85d754f7b945316
Tanbir Hossain <mdtanbirhossain1433@gmail.com>
2024-12-14 01:05:51 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/jahid_ceommerce/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jahid_ceommerce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'Provider/add_to_cart_provider.dart';
+import 'screens/nav_bar_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MultiProvider(
+        providers: [
+          // for add to cart 
+          ChangeNotifierProvider(create: (_)=>CartProvider()),
+
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            textTheme: GoogleFonts.mulishTextTheme(),
+          ),
+          home: const BottomNavBar(),
+        ),
+      );
+}
